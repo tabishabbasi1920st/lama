4bb621bc86f849eba9dcf9db257456e706b8a31b
tabishabbasi1920st <tabishabbasi1920st@gmail.com>
2024-06-07 23:11:19 +0530

    Added navbar in home & static design of home page

---
+++ src/components/Navbar.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const logoUrl =
+  "https://res.cloudinary.com/dctfbwk0m/image/upload/v1717774516/directright_evbeu5.png";
+
+const navLinks = [
+  {
+    id: "SETTINGS",
+    route: "/settings",
+    iconUrl:
+      "https://res.cloudinary.com/dctfbwk0m/image/upload/v1717775096/Icon_1_joejtk.png",
+  },
+  {
+    id: "NOTIFICATION",
+    route: "/notification",
+    iconUrl:
+      "https://res.cloudinary.com/dctfbwk0m/image/upload/v1717775179/notifications_fkjzkr.png",
+  },
+];
+
+const Navbar = () => {
+  const getLinkItem = (link) => {
+    const { id, route, iconUrl } = link;
+    return (
+      <Link to={route}>
+        <li key={id}>
+          <LinkIcon src={iconUrl} alt={id} />
+        </li>
+      </Link>
+    );
+  };
+
+  return (
+    <MainContainer>
+      <LogoImg src={logoUrl} alt="logo" />
+      <LogoTxt>LAMA.</LogoTxt>
+      <LinksContainer>
+        {navLinks.map((eachLink) => getLinkItem(eachLink))}
+      </LinksContainer>
+    </MainContainer>
+  );
+};
+
+export default Navbar;
+
+const MainContainer = styled.nav`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px 0px 10px;
+`;
+
+const LogoImg = styled.img`
+  height: 40px;
+  width: 40px;
+  margin-right: 5px;
+`;
+
+const LogoTxt = styled.h1`
+  color: #7e22ce;
+  font-family: "Plus Jakarta sans";
+`;
+
+const LinksContainer = styled.ul`
+  height: 100%;
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const LinkIcon = styled.img`
+  height: 35px;
+  width: 35px;
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,151 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+
+const bannerImgUrl =
+  "https://res.cloudinary.com/dctfbwk0m/image/upload/v1717779687/Group_16_bwhew7.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  const renderBackToHomeButton = () => {
+    const homeIconUrl =
+      "https://res.cloudinary.com/dctfbwk0m/image/upload/v1717777341/home_1_xnwme8.png";
+
+    return (
+      <Link to={"/home"}>
+        <BackToHomeBtn>
+          <img className="home-icon" src={homeIconUrl} alt={"home icon"} />
+          <span>Back to Home</span>
+        </BackToHomeBtn>
+      </Link>
+    );
+  };
+
+  const renderCreateNewProjectButton = () => {
+    const plusIconUrl =
+      "https://res.cloudinary.com/dctfbwk0m/image/upload/v1717780843/Vector_bw3yo2.png";
+
+    return (
+      <Button>
+        <img className="plus-icon" src={plusIconUrl} alt="plus" />
+        <span>Create New Project</span>
+      </Button>
+    );
+  };
+
+  return (
+    <MainContainer>
+      <header>
+        <Navbar />
+      </header>
+      <Main>
+        {renderBackToHomeButton()}
+        <h1 className="main-heading">Create a New Project</h1>
+        <img className="banner-img" src={bannerImgUrl} alt={"banner"} />
+        <p className="banner-para">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in
+        </p>
+        {renderCreateNewProjectButton()}
+      </Main>
+    </MainContainer>
+  );
 };
 
 export default Home;
+
+const MainContainer = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background-color: #ffffff;
+
+  @media screen and (min-width: 1024px) {
+    max-height: 100vh;
+    overflow: hidden;
+  }
+`;
+
+const Main = styled.main`
+  padding: 10px;
+
+  .main-heading {
+    color: #7e22ce;
+    font-family: "Roboto", sans-serif;
+    font-size: 40px;
+    text-align: center;
+    margin: 10px 0px 10px 0px;
+  }
+
+  .banner-img {
+    height: 40vh;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+  }
+
+  .banner-para {
+    padding: 0px 10% 0px 10%;
+    text-align: center;
+    font-family: "Roboto";
+    font-size: 1.5em;
+    color: #838383;
+  }
+`;
+
+const BackToHomeBtn = styled.button`
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  padding: 3px 15px 3px 15px;
+  border-radius: 15px;
+  font-family: "Roboto", sans-serif;
+  color: #3c3c3c;
+  border: 0.75px solid #999999;
+  font-weight: 500;
+  font-size: 16px;
+  background: #ffffff;
+  box-shadow: 1px 5px 5px 0.1px rgba(126, 34, 206, 0.1);
+  cursor: pointer;
+  &:hover {
+    box-shadow: 1px 5px 5px 0.1px rgba(126, 34, 206, 0.2);
+  }
+
+  .home-icon {
+    height: 25px;
+    width: 25px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #211935;
+  color: #f8f8f8;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  height: 45px;
+  width: 215px;
+  font-family: "Roboto";
+  font-weight: 400;
+  border-radius: 5px;
+  border: none;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  font-size: 18px;
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(33, 25, 53, 0.95);
+  }
+
+  .plus-icon {
+    height: 25px;
+  }
+`;
